db4f88b8e55cc9755a50eb674c0830c5e912326e
mahfuz <mahfuzurrahmanarif@gmail.com>
2022-02-18 00:09:45 +0600

    second commit

--- content.js
+++ content.js
@@ -2,20 +2,21 @@
 //Software Engineer, Agami Soft Ltd.
 
 window.onload = () => {
-	console.log("global hello");
 	if (window.location.href.includes("presentation")) {
 		const btn = document.createElement("button");
-		btn.textContent = "Toggle";
-		btn.title = "show/hide sidebar";
+		btn.textContent = "toggle";
+		btn.title = "Hide Sidebar";
+		btn.style.fontFamily = "Ubuntu";
 		btn.style.cursor = "pointer";
-		btn.style.fontSize = "1rem";
+		btn.style.fontSize = "0.9rem";
 		btn.style.background = "#FBBD3D";
 		btn.style.borderColor = "white";
-		btn.style.color = "black";
-		btn.style.padding = "3px 5px";
+		btn.style.color = "#1c1d1e";
+		btn.style.padding = "4px 5px";
 		btn.style.textAlign = "center";
 		btn.style.display = "inline-block";
-		btn.style.borderRadius = "7px";
+		btn.style.border = "none";
+		btn.style.borderRadius = "5px";
 		btn.style.marginRight = "20px";
 
 		let docs_toolbar = document.getElementById("docs-toolbar");
@@ -27,8 +28,10 @@ window.onload = () => {
 		btn.addEventListener("click", () => {
 			let cur_state = _filmstrip[0].style.display;
 			if (cur_state == "block") {
+				btn.title = "Show Sidebar";
 				_filmstrip[0].style.display = "none";
 			} else {
+				btn.title = "Hide Sidebar";
 				_filmstrip[0].style.display = "block";
 			}
 		});
